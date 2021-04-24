99c708e8ec5438b564865d2257170819881bb0ce
Azydov Sanal <sanal88@yandex.ru>
2021-04-24 23:02:15 +0300

    Added glew library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(){
+
+#include <iostream>
+
+int main()
+{
   //Инициализация GLFW
   glfwInit();
   //Настройка GLFW
@@ -13,6 +18,22 @@ int main(){
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   //Выключение возможности изменения размера окна
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK)
+  {
+    std::cout << "Failed to initialize GLEW" << std::endl;
+    return -1;
+  }
 
   return 0;
 }
