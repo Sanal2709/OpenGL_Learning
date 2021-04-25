599e6e08a38f81cd1a97fe774e35568d51dc1aa2
Azydov Sanal <sanal88@yandex.ru>
2021-04-25 17:53:46 +0300

    Added triangle

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 
 #include <iostream>
 
-#define WINDOW_WIDTH (800)
-#define WINDOW_HEIGHT (600)
+// Window dimensions
+const GLuint WINDOW_WIDTH = (800);
+const GLuint WINDOW_HEIGHT = (600);
+
+// Shaders
+const GLchar *vertexShaderSource = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 position;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+                                   "}\0";
+const GLchar *fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 color;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\n\0";
 
 void esc_key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
@@ -13,15 +28,17 @@ void esc_key_callback(GLFWwindow *window, int key, int scancode, int action, int
     glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-void interpolate_colors(GLclampf& r, GLclampf& g, GLclampf& b){
+void interpolate_colors(GLclampf &r, GLclampf &g, GLclampf &b)
+{
   //it's kind we walk around color interpolated triangle
   //all distance == 300
   //step == 1
 
   const int all_distance = 300;
-  const int edge_distance = all_distance/3;
+  const int edge_distance = all_distance / 3;
   static int curr_pos = -1;
-  if(curr_pos++ >= all_distance -1){
+  if (curr_pos++ >= all_distance - 1)
+  {
     curr_pos = 0;
   }
 
@@ -31,16 +48,19 @@ void interpolate_colors(GLclampf& r, GLclampf& g, GLclampf& b){
   GLclampf right_border_r = 0.0f;
   GLclampf right_border_g = 0.0f;
   GLclampf right_border_b = 0.0f;
-  
-  if(curr_pos < edge_distance){
+
+  if (curr_pos < edge_distance)
+  {
     left_border_r = 1.0f;
     right_border_g = 1.0f;
   }
-  else if(curr_pos < edge_distance*2){
+  else if (curr_pos < edge_distance * 2)
+  {
     left_border_g = 1.0f;
     right_border_b = 1.0f;
   }
-  else {
+  else
+  {
     left_border_b = 1.0f;
     right_border_r = 1.0f;
   }
@@ -87,12 +107,77 @@ int main()
 
   int width, height;
   glfwGetFramebufferSize(window, &width, &height);
-
   glViewport(0, 0, width, height);
+
+  // Build and compile our shader program
+  // Vertex shader
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  // Check for compile time errors
+  GLint success;
+  GLchar infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // Fragment shader
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  // Check for compile time errors
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // Link shaders
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  // Check for linking errors
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // Set up vertex data (and buffer(s)) and attribute pointers
+  GLfloat vertices[] = {
+      -0.5f, -0.5f, 0.0f, // Left
+      0.5f, -0.5f, 0.0f,  // Right
+      0.0f, 0.5f, 0.0f    // Top
+  };
+  GLuint VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+  glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
 
   GLclampf red = 0.0f;
   GLclampf green = 0.0f;
   GLclampf blue = 0.0f;
+
 
   while (!glfwWindowShouldClose(window))
   {
@@ -104,10 +189,19 @@ int main()
     glClearColor(red, green, blue, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // Draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
+
     //swap buffers
     glfwSwapBuffers(window);
   }
 
+  // Properly de-allocate all resources once they've outlived their purpose
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
   glfwTerminate();
 
   return 0;
